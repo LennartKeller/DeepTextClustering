2d049e67072a48d8fcbbf5a2824bf939dace7b1b
Lennart Keller <lennart.keller@googlemail.com>
2020-11-27 17:34:10 +0100

    restructure project

--- misc/make_ag_news_subset_10.py
+++ misc/make_ag_news_subset_10.py
@@ -1,4 +1,4 @@
-from src.utils.datasets import make_ag_news_subset
+from src import make_ag_news_subset
 
 if __name__ == '__main__':
     make_ag_news_subset('../datasets/ag_news_subset10.csv', subset_size=0.1)
