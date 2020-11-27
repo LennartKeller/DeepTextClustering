7b8cb38be54d8b65dbab7856988d2c8c21273f6b
Lennart Keller <lennart.keller@googlemail.com>
2020-11-27 17:30:53 +0100

    add stuff

---
+++ model/utils/datasets.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import tensorflow_datasets as tfds
+from itertools import chain
+from sklearn.model_selections import train_test_split
+
+
+def make_ag_news_subset(out_path: str, subset_size=0.1, random_state=42):
+
+    """
+    Create a subset of the ag_news dataset as found in tf-datasets.
+    :param out_path:
+    :param subset_size:
+    :param random_state:
+    :return:
+    """
+    train_ds = tfds.load('ag_news_subset', split='train', shuffle_files=True)
+    test_ds = tfds.load('ag_news_subset', split='test', shuffle_files=True)
+
+    texts, labels = [], []
+
+    for ds in (train_ds, test_ds):
+       for example in tfds.as_numpy(ds):
+           text, label = example['description'], example['label']
+           texts.append(text.decode("utf-8"))
+           labels.append(label)
+
+    labels = np.array(labels)
+
+    test_subset, _, labels_subset, _ = train_test_split(texts, labels,
+                                                       test_size=1-subset_size,
+                                                       random_state=random_state)
+
+    df = pd.DataFrame()
+    df['texts'] = texts
+    df['labels'] = labels
+
+    if out_path:
+        df.to_csv(out_path)
+
+    return df
