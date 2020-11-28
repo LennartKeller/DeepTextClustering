4d09e34f257ef77fc9f91dfd452e3cd54d378928
Lennart Keller <lennart.keller@googlemail.com>
2020-11-28 17:34:58 +0100

    add model

--- src/deep_kmeans/lm_deep_kmeans.py
+++ src/deep_kmeans/lm_deep_kmeans.py
@@ -1,7 +1,104 @@
+from dataclasses import dataclass
+
+import torch
 from torch import nn
+from transformers.file_utils import ModelOutput
+
+from .utils.distance import lp_distance
+from .utils.language_modeling import mask_tokens
 
 
-class DeepKMeansWithLM(nn.Module):
+@dataclass
+class ClusterOutput(ModelOutput):
+    loss: torch.FloatTensor = None
+    predicted_labels: torch.IntTensor = None
+    embeddings: torch.FloatTensor = None
 
-    def __init__(self, initial_centroids, lm_model, tokenizer):
-        super(DeepKMeansWithLM, self).__init__()
+
+class ClusterLM(nn.Module):
+
+    def __init__(self,
+                 initial_centroids: torch.tensor,
+                 lm_model,
+                 tokenizer,
+                 metric=lp_distance,
+                 do_language_modeling=True,
+                 device='cpu'
+                 ):
+        super(ClusterLM, self).__init__()
+
+        self.initial_centroids = initial_centroids
+
+        self.add_module('lm_model', lm_model)
+        self.register_parameter('centroids', nn.Parameter(initial_centroids.clone().float(), requires_grad=True))
+
+        self.tokenizer = tokenizer
+        self.metric = metric
+        self.do_language_modeling = do_language_modeling
+        self.device = device
+
+        self.to(self.device)
+
+    def forward(self, texts, alpha=1.0):
+        """
+        Input: texts and labels (optional)
+        Returns: lm_language modelling output, own output dict (clustering_loss, predicted_labels)
+        """
+        # Language Modeling Part:
+
+        lm_outputs = None
+
+        if self.do_language_modeling:
+            inputs = self.tokenizer(
+                texts,
+                return_tensors='pt',
+                padding=True,
+                truncation=True)
+
+            input_ids = inputs['input_ids'].clone()
+            input_ids, true_ids = mask_tokens(input_ids, self.tokenizer)
+            inputs['input_ids'] = input_ids
+
+            inputs = inputs.to(self.device)
+            true_ids = true_ids.to(self.device)
+            lm_outputs = self.lm_model(labels=true_ids, **inputs)
+
+        # Clustering Part:
+        inputs = self.tokenizer(
+            texts,
+            return_tensors='pt',
+            padding=True,
+            truncation=True)
+
+        inputs.to(self.device)
+
+        # 0. Obtain embeddings for each input
+        input_embeddings = self.lm_model.base_model(**inputs).last_hidden_state[:, 0, :].float()
+
+        # 1. Compute distances from each input embedding to each centroids
+        distances = torch.stack([self.metric(embedding.unsqueeze(0), self.centroids) for embedding in input_embeddings])
+        nearest_centroids = torch.argmin(distances.cpu().clone().detach(), dim=1)
+        distances = torch.transpose(distances, 0, 1)  # => shape (n_centroids, n_samples)
+
+        # 2. Compute the paramterized softmin for each centroid of each distance to each centroid per input sample
+        # Find min distances for each centroid
+        min_distances = torch.min(distances, dim=1).values
+        # Compute exponetials
+        exponentials = torch.exp(- alpha * (distances - min_distances.unsqueeze(1)))
+        # Compute softmin
+        softmin = exponentials / torch.sum(exponentials, dim=1).unsqueeze(1)
+
+        # 3. Weight the distance between each sample and each centroid
+        weighted_distances = distances * softmin
+
+        # 4. Sum over weighted_distances to obtain loss
+        clustering_loss = weighted_distances.sum(dim=1).mean()
+
+        # Create clustering output dictionary
+        cluster_outputs = ClusterOutput(
+            loss=clustering_loss,
+            predicted_labels=nearest_centroids.long(),
+            embeddings=input_embeddings.cpu().detach()
+        )
+
+        return lm_outputs, cluster_outputs
