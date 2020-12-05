ecfab1320db28913493fbbc1cff40f0e5fc4b696
Lennart Keller <lennart.keller@googlemail.com>
2020-12-05 17:00:12 +0100

    restructure module

---
+++ DeepKMeans/helpers.py
@@ -0,0 +1,107 @@
+from typing import Optional
+
+import numpy as np
+import torch
+from scipy.optimize import linear_sum_assignment
+from torch.utils.data import Dataset
+
+
+def cluster_accuracy(y_true, y_predicted, cluster_number: Optional[int] = None):
+    """
+    Calculate clustering accuracy after using the linear_sum_assignment function in SciPy to
+    determine reassignments.
+    :param y_true: list of true cluster numbers, an integer array 0-indexed
+    :param y_predicted: list  of predicted cluster numbers, an integer array 0-indexed
+    :param cluster_number: number of clusters, if None then calculated from input
+    :return: reassignment dictionary, clustering accuracy
+    """
+    if cluster_number is None:
+        cluster_number = (
+                max(y_predicted.max(), y_true.max()) + 1
+        )  # assume labels are 0-indexed
+    count_matrix = np.zeros((cluster_number, cluster_number), dtype=np.int64)
+    for i in range(y_predicted.size):
+        count_matrix[y_predicted[i], y_true[i]] += 1
+
+    row_ind, col_ind = linear_sum_assignment(count_matrix.max() - count_matrix)
+    reassignment = dict(zip(row_ind, col_ind))
+    accuracy = count_matrix[row_ind, col_ind].sum() / y_predicted.size
+    return reassignment, accuracy
+
+
+class TextDataset(Dataset):
+    def __init__(self, texts, labels):
+        self.texts = texts
+        self.labels = labels
+
+        self.labels = torch.tensor(self.labels, dtype=torch.float)
+
+    def __getitem__(self, index):
+        return self.texts[index], self.labels[index]
+
+    def __len__(self):
+        return len(self.texts)
+
+
+def lp_distance(X, Y, p=2):
+    """
+    Computes row wise minkowski distances between matrices X and Y
+    """
+    return torch.sum(torch.abs(X - Y) ** p, dim=1) ** (1 / p)
+
+
+def cosine_distance(X, Y):
+    """
+    Computes the rowwise cosine distance between matrices X and Y.
+    """
+    dots = torch.stack([torch.dot(xi, yi) for xi, yi in zip(X, Y)])
+    norms = torch.stack(
+        [torch.dot(xi.unsqueeze(0), yi.unsqueeze(0)) for xi, yi in zip(torch.norm(X, dim=1), torch.norm(Y, dim=1))])
+    cos_sim = torch.div(dots, norms)
+    return 1.0 - cos_sim
+
+
+from typing import *
+
+
+def mask_tokens(inputs: torch.Tensor, tokenizer) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+    """
+
+    if tokenizer.mask_token is None:
+        raise ValueError(
+            "This tokenizer does not have a mask token which is necessary for masked language modeling. Remove the --mlm flag if you want to use this tokenizer."
+        )
+
+    labels = inputs.clone()
+    # We sample a few tokens in each sequence for masked-LM training (with probability args.mlm_probability defaults to 0.15 in Bert/RoBERTa)
+    probability_matrix = torch.full(labels.shape, 0.15)
+    special_tokens_mask = [
+        tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+    ]
+    probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
+    if tokenizer._pad_token is not None:
+        padding_mask = labels.eq(tokenizer.pad_token_id)
+        probability_matrix.masked_fill_(padding_mask, value=0.0)
+    masked_indices = torch.bernoulli(probability_matrix).bool()
+    labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+    # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+    inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+
+    # 10% of the time, we replace masked input tokens with random word
+    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    inputs[indices_random] = random_words[indices_random]
+
+    # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+    return inputs, labels
+
+
+def mean_sparsity(X):
+    """
+    Computes the mean of the row-wise sparsity of the input feature-matrix X
+    """
+    return np.mean(np.count_nonzero(X, axis=1) / X.shape[1])
