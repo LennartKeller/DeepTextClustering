9f62f2563458685365da7dd52398e994769564be
Lennart Keller <lennart.keller@googlemail.com>
2020-12-07 20:31:28 +0100

    change earlystopping param in subset10 and add full ag_news exp

--- experiments/ag_news-distilbert.py
+++ experiments/ag_news-distilbert.py
@@ -3,10 +3,11 @@ import pickle
 from time import gmtime, strftime
 
 import numpy as np
-import pandas as pd
+import tensorflow_datasets as tfds
 import torch
 from sacred import Experiment
 from sacred.observers import FileStorageObserver
+from sklearn.utils import shuffle
 from torch.utils.data import DataLoader
 from transformers import AutoModelForMaskedLM, AutoTokenizer
 from transformers import get_linear_schedule_with_warmup
@@ -14,7 +15,7 @@ from transformers import get_linear_schedule_with_warmup
 from transformers_clustering.helpers import TextDataset
 from transformers_clustering.model import init_model, train, concat_cls_n_hidden_states
 
-ex = Experiment('ag_news_subset10-distilbert')
+ex = Experiment('ag_news-distilbert')
 ex.observers.append(FileStorageObserver('../results/sacred_runs'))
 
 
@@ -27,8 +28,7 @@ def cfg():
     clustering_loss_weight = 1.0
     embedding_extractor = concat_cls_n_hidden_states
     annealing_alphas = np.arange(1, n_epochs + 1)
-    dataset = "../datasets/ag_news_subset10.csv"
-    result_dir = f"../results/ag_news_subset10-distilbert/{strftime('%Y-%m-%d_%H:%M:%S', gmtime())}"
+    result_dir = f"../results/ag_news-distilbert/{strftime('%Y-%m-%d_%H:%M:%S', gmtime())}"
     val_size = 0.1  # not used
     early_stopping = True
     early_stopping_tol = 0.01
@@ -44,7 +44,6 @@ def run(n_epochs,
         clustering_loss_weight,
         embedding_extractor,
         annealing_alphas,
-        dataset,
         result_dir,
         val_size,
         early_stopping,
@@ -58,10 +57,19 @@ def run(n_epochs,
     torch.cuda.manual_seed_all(random_state)
 
     # load data
-    df = pd.read_csv(dataset)
+    train_ds = tfds.load('ag_news_subset', split='train', shuffle_files=True)
+    test_ds = tfds.load('ag_news_subset', split='test', shuffle_files=True)
+    texts, labels = [], []
+    for ds in (train_ds, test_ds):
+        for example in tfds.as_numpy(ds):
+            text, label = example['description'], example['label']
+            texts.append(text.decode("utf-8"))
+            labels.append(label)
+    labels = np.array(labels)
+    del train_ds
+    del test_ds
 
-    texts = df['texts'].to_numpy()
-    labels = df['labels'].to_numpy()
+    text, labels = shuffle(texts, labels, random_state=random_state)
 
     data = TextDataset(texts, labels)
     data_loader = DataLoader(dataset=data, batch_size=batch_size, shuffle=False)
@@ -117,5 +125,3 @@ def run(n_epochs,
         pickle.dump(hist, file=f)
 
     torch.save(model, os.path.join(result_dir, 'model.bin'))
-
-
