73e509b782dacbe32d9441ecc8b7d965c55025a4
Lennart Keller <lennart.keller@googlemail.com>
2020-12-16 15:47:40 +0100

    update trec6 dataset creation

--- datasets/trec6/make_trec6_dataset.py
+++ datasets/trec6/make_trec6_dataset.py
@@ -1,17 +1,19 @@
 import numpy as np
 import pandas as pd
-import tensorflow_datasets as tfds
+import requests
+
 
 if __name__ == '__main__':
-    train_ds = tfds.load('trec', split='train', shuffle_files=True)
-    test_ds = tfds.load('trec', split='test', shuffle_files=True)
+    r = requests.get('https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label')
+    data = r.text
     texts, labels = [], []
-    for ds in (train_ds, test_ds):
-        for example in tfds.as_numpy(ds):
-            text, label = example['description'], example['label-coarse']
-            texts.append(text.decode("utf-8"))
-            labels.append(label)
-    labels = np.array(labels)
+    for line in data.split('\n'):
+        tokens = line.split()
+        if len(tokens) >= 2:
+            l, t = tokens[0], tokens[1:]
+            l = l.split(':')[0]  # we discard the fine labels
+            texts.append(" ".join(t))
+            labels.append(l)
 
     save = pd.DataFrame()
     save['texts'] = texts
