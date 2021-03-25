2ed3f7bf7e96b2b08ac71adf6e04857f838e3d5f
Lennart Keller <lennart.keller@googlemail.com>
2021-03-25 12:06:39 +0100

    fix

--- experiments/run_final_experiments.sh
+++ experiments/run_final_experiments.sh
@@ -3,13 +3,12 @@
 eval "$(conda shell.bash hook)"
 conda activate clustering
 
-export CUDA_VISIBLE_DEVICES="1,2,3,4"
+export CUDA_VISIBLE_DEVICES="0,1,2,4"
 export MONGO_SACRED_ENABLED=false
 
 python ag_news_subset5-distilbert.py with device='cuda:1'
 python ag_news_subset5-bert-base.py with device='cuda:1'
 python trec6-distilbert.py with device='cuda:1'
 python trec6-bert-base.py with device='cuda:1'
-python trec6-distilbert.py with device='cuda:1'
 python 20_newsgroups-distilbert.py with device='cuda:1'
 python 20_newsgroups-bert-base.py with device='cuda:1'
