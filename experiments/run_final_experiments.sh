18f2f03664877f7bb99c300fa3fe4d344a967910
Lennart Keller <lennart.keller@googlemail.com>
2021-03-25 12:13:09 +0100

    change gpu

--- experiments/run_final_experiments.sh
+++ experiments/run_final_experiments.sh
@@ -6,9 +6,9 @@ conda activate clustering
 export CUDA_VISIBLE_DEVICES="0,1,2,4"
 export MONGO_SACRED_ENABLED=false
 
-python ag_news_subset5-distilbert.py with device='cuda:1'
-python ag_news_subset5-bert-base.py with device='cuda:1'
-python trec6-distilbert.py with device='cuda:1'
-python trec6-bert-base.py with device='cuda:1'
-python 20_newsgroups-distilbert.py with device='cuda:1'
-python 20_newsgroups-bert-base.py with device='cuda:1'
+python ag_news_subset5-distilbert.py with device='cuda:2'
+python ag_news_subset5-bert-base.py with device='cuda:2'
+python trec6-distilbert.py with device='cuda:2'
+python trec6-bert-base.py with device='cuda:2'
+python 20_newsgroups-distilbert.py with device='cuda:2'
+python 20_newsgroups-bert-base.py with device='cuda:2'
